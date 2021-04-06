7cf5b2ca2ebf89e25457693a1e74aa7a0804179e
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-04-06 21:48:23 +0200

    fixed redux logger on prod

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(
   rootReducer,
-  applyMiddleware(logger, sagaMiddleware)
+  applyMiddleware(...middlewares)
 );
 
 interceptors(store);
