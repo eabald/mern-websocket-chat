b2e5117c9eefd9a245b8878efb2eced0238ef5a5
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-04-13 23:27:33 +0200

    saving sockets ids to user model

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -9,7 +9,7 @@ import messageModel from '../models/message.model';
 import SocketWithUser from '../interfaces/socketWithUser.interface';
 import userModel from './../models/user.model';
 import roomModel from '../models/room.model';
-import passportSocketIo from 'passport.socketio'
+import passportSocketIo from 'passport.socketio';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 
@@ -38,20 +38,38 @@ class MessageController implements Controller {
       createAdapter({ pubClient: this.pubClient, subClient: this.subClient })
     );
     const RedisStore = connectRedis(session);
-    this.websocket.use(passportSocketIo.authorize({
-      secret: process.env.SESSION_SECRET,
-      store: new RedisStore({
-        client: new RedisClient({
-          host: process.env.REDIS_HOST,
-          port: Number(process.env.REDIS_PORT),
+    this.websocket.use(
+      passportSocketIo.authorize({
+        secret: process.env.SESSION_SECRET,
+        store: new RedisStore({
+          client: new RedisClient({
+            host: process.env.REDIS_HOST,
+            port: Number(process.env.REDIS_PORT),
+          }),
+          disableTouch: true,
         }),
-        disableTouch: true,
-      }),
-    }));
+      })
+    );
+
+    this.websocket.use(this.setSocketId);
     this.websocket.on('connection', (socket: SocketWithUser) =>
       this.bindSocketEvents(socket, this.websocket)
     );
   }
+
+  private setSocketId = async (
+    socket: SocketWithUser,
+    next: () => void
+  ): Promise<void> => {
+    let user = await this.user.findById(socket.request.user._id);
+    if (user && user.socketId !== socket.id) {
+      user.socketId = socket.id;
+      user = await user.save();
+      next();
+    } else {
+      next();
+    }
+  };
 
   private bindSocketEvents(
     socket: SocketWithUser,
@@ -73,14 +91,16 @@ class MessageController implements Controller {
     websocket: socketio.Server
   ): Promise<void> {
     let newMessage = await this.message.create(message);
-    newMessage = await newMessage.populate('user').execPopulate()
+    newMessage = await newMessage.populate('user').execPopulate();
     const room = await this.room.findById(message.room._id);
-    const isBlocked = room.users.some(user => socket.request.user.blockedBy.includes(user));
+    const isBlocked = room.users.some((user) =>
+      socket.request.user.blockedBy.includes(user)
+    );
     if (room.type === 'dm' && isBlocked) {
       console.log('here');
       socket.emit('messageBlocked', {
         status: 'blocked',
-        message: 'Sending message to this user has been blocked.'
+        message: 'Sending message to this user has been blocked.',
       });
       return;
     }
@@ -88,7 +108,9 @@ class MessageController implements Controller {
     await room.save();
     const usersInRoom = await this.user.find({ _id: { $in: room.users } });
     usersInRoom.forEach(async (user) => {
-      const alreadyUnread = user.unread.map(item => item._id.toString()).includes(message.room._id);
+      const alreadyUnread = user.unread
+        .map((item) => item._id.toString())
+        .includes(message.room._id);
       if (message.user.id !== user.id && !alreadyUnread) {
         user.unread.push(message.room);
         await user.save();
